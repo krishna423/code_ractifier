fe1ed04c3a18e0c98c32f063ea7364b42cfe9c9b
GitHub Action <action@github.com>
2020-08-12 17:42:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/krishna423/coderactifier/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/krishna423/coderactifier/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.krishna423.coderactifier.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.krishna423.coderactifier.MyBundle
 
 class MyApplicationService {
 
